00afa12113817b49de0c59d09d1707fdf418a4f7
Paul Staroch <paulchen@rueckgr.at>
2022-10-13 21:18:03 +0200

    add switch --no-systemd to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,7 @@ docker pull ubuntu:jammy || exit 1
 
 docker build . -t docker-munin-mongodb --no-cache || exit 1
 
-sudo systemctl restart docker-munin-mongodb
+if [ "$1" != "--no-systemd" ]; then
+	sudo systemctl restart docker-munin-mongodb
+fi
 
